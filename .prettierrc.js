06158f743a6d1ba49624f4f30a85e3d608dbd9d8
Ivan Dashkevich <ivandashk.public@gmail.com>
2025-01-27 19:18:26 +0100

    Добавить только конфиг для prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  htmlWhitespaceSensitivity: 'strict',
+  trailingComma: 'es5',
+  arrowParens: 'always',
+};
