df66f304823732e000d7326a7c133c00219a39aa
Shkoterman <chkoterman@gmail.com>
2025-02-16 16:41:56 +0100

    Добавил динамические title и description

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import type { Metadata } from 'next';
 
 import { mapAirtableEvents } from '@/api/airtable/mapAirtableEvents';
 import { fetchOneAirtableEvent } from '@/api/airtable/fetchOneAirtableEvent';
 import { EventPage } from './EventPage';
 
 export const dynamic = 'force-dynamic';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug } = await params; // Разрешаем промис
+  const data = await fetchOneAirtableEvent({ eventId: slug });
+  const mappedData = mapAirtableEvents(data.records);
+  const airtableEvent = mappedData.events.at(0);
+
+  if (!airtableEvent) {
+    return {
+      title: 'ensalada - сообщество в Барселоне',
+      description: 'Комьюнити в Барселоне',
+    };
+  }
+
+  return {
+    title: 'ensalada - ' + airtableEvent.title,
+    description: airtableEvent.description,
+  };
+}
 
 export default async function Event({
   params,
